6fc6a276eb2e642d7c8aa91068cbfb60c9dcec69
ntpeters <ntpeters@mtu.edu>
2014-01-13 09:25:16 -0500

    Add my own pretty backtrace support

--- backtrace-symbols.h
+++
@@ -1,8 +0,0 @@
-/*
-	Header file for modified 'backtrace-symbols.c' by Jeff Muizelaar
-
-	Nathan Peterson 2014
-*/
-
-char **backtrace_symbols(void *const *buffer, int size);
-void backtrace_symbols_fd(void *const *buffer, int size, int fd);
